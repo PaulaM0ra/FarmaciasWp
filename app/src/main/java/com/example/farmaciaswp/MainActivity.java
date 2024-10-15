b9d744d05d5cfd020c5ee4fa2384d9fe4d8378bc
Wilsonm123_@hotmail.com <wilsonm123_@hotmail.com>
2024-10-14 23:16:56 -0500

    Buttons- Flotante / Rsgistro Producto

--- app/src/main/java/com/example/farmaciaswp/MainActivity.java
+++ app/src/main/java/com/example/farmaciaswp/MainActivity.java
@@ -1,15 +1,14 @@
 package com.example.farmaciaswp;
 
-import android.content.Intent;  // Asegúrate de importar esta clase
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.view.View; // Importa esta clase para usar OnClickListener
-import android.widget.Button; // Importa esta clase para trabajar con los botones
-
+import android.view.View;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -18,21 +17,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Asegúrate de que esto funcione con tu layout actual
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // Aquí añadimos el código para el botón "NEXT"
-        Button nextButton = findViewById(R.id.button12);  // Encuentra el botón "NEXT"
+
+        Button nextButton = findViewById(R.id.button12);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Crear el Intent para cambiar a ProductActivity
+
                 Intent intent = new Intent(MainActivity.this, ProductActivity.class);
-                startActivity(intent);  // Inicia ProductActivity
+                startActivity(intent);
             }
         });
     }
