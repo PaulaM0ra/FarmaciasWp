f17d3f844e3d1443104a31102af605bfcbbc407d
pdmora <dayismora1804@gmail.com>
2024-11-08 20:31:34 -0500

    funciona

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,12 +53,9 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
-    implementation ("com.google.firebase:firebase-database:20.3.1")
-    implementation ("com.google.firebase:firebase-storage:20.0.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.google.firebase:firebase-storage:20.0.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
 }
-
-apply(plugin = "com.google.gms.google-services")
-
+    apply(plugin = "com.google.gms.google-services")
